3381d6d41c2023b2add48adf869a8b8fda309a0b
lee <test@gmail.com>
2025-08-18 13:35:37 +0300

    .

--- assignments/1.1/src/Main.java
+++ assignments/1.1/src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello World!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                " ");
+
+    }
+
+}
+class SumOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("The sum of the numbers is " + (first + second + third));
+        System.out.println("The product of the numbers is " + (first * second * third));
+        double avg = (double)(first + second + third)/3;
+        System.out.println("The average of the numbers is " + avg);
+        //The data type should be double to contain the decimals
+    }
+}
+
+class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println("The cat named " + name + " says: Meow!");
+    }
+
+    public static void main(String[] args) {
+        // Create an instance of the Cat class with a name
+        Cat cat = new Cat("Whiskers");
+        Cat cat2 = new Cat("Rex");
+
+        // Call the meow method on the cat instance
+        cat.meow();
+        cat.meow();
+        cat2.meow();
+        cat.meow();
     }
 }
