2e055549c93bc334bcf9b7c1827e334b7c8178f6
lee <test@gmail.com>
2025-08-19 13:59:42 +0300

    .

---
+++ assignments/1.4/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        String[] firstnames = {"James","Michael","John","Robert","David","William"};
+        String[] lastnames = {"Elsher","Solace","Levine","Thatcher","Raven","Bardot"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of names:");
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            int randomNum1 = (int) (Math.random() * 6);
+            int randomNum2 = (int) (Math.random() * 6);
+            System.out.println(firstnames[randomNum1]+" "+lastnames[randomNum2]);
+        }
+    }
+}
+
+class subarray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the integers into the array:");
+        for(int i=0;i<n;i++){
+            System.out.println("Enter integer "+ (i+1) +" :" );
+            nums[i] = sc.nextInt();
+        }
+        int sum=0;
+        int[][] subarray = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                subarray[i][j]=sum;
+            }
+            sum=0;
+        }
+
+        int max=0,maxA=0,maxB=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(subarray[i][j]>max){
+                    max=subarray[i][j];
+                    maxA=i+1;
+                    maxB=j+1;
+                }
+            }
+        }
+        System.out.println("The maximum number is "+max);
+        System.out.println("Integers "+maxA+"-"+maxB);
+
+    }
+}
+
+class duplicates{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        boolean duplicate=false;
+        int count=0;
+        System.out.println("Enter the integers into the array:");
+        for(int i=0;i<n;i++){
+            System.out.println("Enter integer "+ (i+1) +" :" );
+            int num = sc.nextInt();
+            for(int j=0;j<i;j++){
+                if(nums[j]==num){
+                    duplicate=true;
+                }
+            }
+            if(!duplicate){
+                nums[count]=num;
+                count++;
+            }
+            duplicate=false;
+        }
+        for(int i=0;i<count;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
